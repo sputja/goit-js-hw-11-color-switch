f27284c0c39e3f082f350de90708fd962c9e5ce1
sputja <sputja@gmail.com>
2020-12-19 16:55:07 +0200

    final

--- js/task-11_color-switch.js
+++ js/task-11_color-switch.js
@@ -20,10 +20,28 @@ const colors = [
   "#795548",
 ];
 
-// const min =0;
-// const max =5;
+const refs = {
+  startBtn: document.querySelector('[data-action = "start"]'),
+  stopBtn: document.querySelector('[data-action = "stop"]'),
+  htmlBody: document.querySelector("body"),
+  intervalColorId: null,
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-console.log(randomIntegerFromInterval(0, 5));
+
+refs.startBtn.addEventListener("click", () => {
+  refs.stopBtn.removeAttribute("disabled");
+  refs.startBtn.setAttribute("disabled", true);
+  refs.intervalColorId = setInterval(() => {
+    refs.htmlBody.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+  refs.startBtn.removeAttribute("disabled");
+  refs.stopBtn.setAttribute("disabled", true);
+  clearInterval(refs.intervalColorId);
+});
